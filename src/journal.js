aa9c721468aca4878c17f292aacb229caf191a0b
donnymays <dtmays2@gmail.com>
2020-09-14 13:22:41 -0700

    beign building index.html form

--- src/journal.js
+++ src/journal.js
@@ -1,5 +1,14 @@
 export default class Journal {
-  constructor(plant) {
-    this.plant = plant;
+  constructor(plants) {
+    this.plants = [];
+    this.currentId = 0;
+  }
+  addContact() {
+    plant.id = this.assignId();
+    this.plants.push(plant);
+  }
+  assignId (){
+    this.currentId += 1;
+    return this.currentId;
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,3 +2,9 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+$(document).ready(function)() {
+  $("").submit(function() {
+    
+  })
+}
