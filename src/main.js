1d8c750aae7158812a9b5f50c4f38135f53e045c
donnymays <dtmays2@gmail.com>
2020-09-14 13:41:15 -0700

    begin main.js work

--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Plant from './plant';
 
 $(document).ready(function)() {
-  $("").submit(function() {
+  $("#addPlant").submit(function() {
+    let inputtedPlantName = $("input#plantName").val();
+    let inputtedPlantType = $(input#plantType).val();
+    let inputtedPlantDate = $(input#plantBirthday).val();
+    let inputtedWaterDay = $(input#WaterDay).val();
+
+    let newPlant = new Plant(inputtedPlantName, inputtedPlantType, inputtedPlantDate, inputtedWaterDay);
     
+
   })
 }
