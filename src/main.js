35131615d21f691b9c3771b17875ab65b3a61da0
donnymays <dtmays2@gmail.com>
2020-09-14 13:55:26 -0700

    successul plant class created

--- src/main.js
+++ src/main.js
@@ -4,15 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Plant from './plant';
 
-$(document).ready(function)() {
+$(document).ready(function() {
   $("#addPlant").submit(function() {
+    event.preventDefault();
+
     let inputtedPlantName = $("input#plantName").val();
-    let inputtedPlantType = $(input#plantType).val();
-    let inputtedPlantDate = $(input#plantBirthday).val();
-    let inputtedWaterDay = $(input#WaterDay).val();
+    let inputtedPlantType = $("input:radio[name=plantType]:checked").val();
+    let inputtedPlantDate = $("input#plantBirthday").val();
+    let inputtedWaterDay = $("input:radio[name=waterDay]:checked").val();
 
     let newPlant = new Plant(inputtedPlantName, inputtedPlantType, inputtedPlantDate, inputtedWaterDay);
-    
+    console.log(newPlant);
 
-  })
-}
+  });
+});
