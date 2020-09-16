13a8e768582b10f950754c22e527e73db11e7a7f
Alex Goodwin <hagoodwi@gmail.com>
2020-09-16 10:38:24 -0700

    fix showPlantCard funcionality

--- __tests__/journal.test.js
+++ __tests__/journal.test.js
@@ -1,13 +1,24 @@
-import Journal from '../src/journal.js';
+import {Journal} from '../src/journal.js';
 import { Plant } from '../src/journal.js';
 
 describe('Journal', () => {
 
-  test('should correctly add a Plant object to the Journal array', () => {
+  test('should correctly add a Plant object to the plants array', () => {
     const plant = new Plant("Mr. Cactus");
+    const plant2 = new Plant("Mr. Flower");
     const journal = new Journal();
-    journal.addPlant();
-    expect(journal.plants.length).toEqual(1);
+    journal.addPlant(plant);
+    journal.addPlant(plant2);
+    expect(journal.plants.length).toEqual(2);
+  }); 
+
+  test('should correctly increase id by one', () => {
+    const plant = new Plant("Mr. Cactus");
+    const plant2 = new Plant("Mr. Flower");
+    const journal = new Journal();
+    journal.addPlant(plant);
+    journal.addPlant(plant2);
+    expect(plant2.id).toEqual(2);
   }); 
 
 });
--- src/main.js
+++ src/main.js
@@ -2,40 +2,26 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Plant from './plant.js';
-import { Journal } from './journal.js';
+// import  Plant from './plant.js';
+import { Journal, Plant } from './journal.js';
 
 
-function removePlant(id) {
-  for (let i=0; i<this.plants.length; i++) {
-    if (this.plants[i]) {
-      if (this.plants[i].id == id) {
-        delete this.plants[i];
-        return true;
-      }
-    }
-  }
-  return false;
-}
-
-function attachPlantListeners() {
-  $('#deleteButton').click(function() {
-    removePlant(this.id);
-  })
-}
-
-function showPlantCard(plantCardToDisplay) {
+function showPlantCard(plant) {
   let plantDiv = $("div#bottomDiv");
   let htmlForPlantCard = "";
-  plantCardToDisplay.plants.forEach(function (plant) {
-    htmlForPlantCard += "<div class='col-md-4'><div class='card plantCard' ><div class='card-body'><h5 id='plantCardTitle' class='card-title'>" + plant.name + "</h5><h6 id='plantCardType' class='card-subtitle mb-2'>" + plant.type + "</h6><p>" + plant.birthday + "</p><p>" + plant.waterDay + "</p><button id='deleteButton' class='btn-dark' type='button'>Remove plant</button>";
-    plantDiv.append(htmlForPlantCard);
-  });
+  htmlForPlantCard += "<div class='col-md-4'><div class='card plantCard' ><div class='card-body'><h5 id='plantCardTitle' class='card-title'>" + plant.name + "</h5><h6 id='plantCardType' class='card-subtitle mb-2'>" + plant.type + "</h6><p>" + plant.birthday + "</p><p>" + plant.waterDay + "</p><button id='deleteButton' class='btn-dark' type='button'>Remove plant</button>";
+  plantDiv.append(htmlForPlantCard);
+  
+  // plantCardToDisplay.plants.forEach(function (plant) {
+  //   htmlForPlantCard += "<div class='col-md-4'><div class='card plantCard' ><div class='card-body'><h5 id='plantCardTitle' class='card-title'>" + plant.name + "</h5><h6 id='plantCardType' class='card-subtitle mb-2'>" + plant.type + "</h6><p>" + plant.birthday + "</p><p>" + plant.waterDay + "</p><button id='deleteButton' class='btn-dark' type='button'>Remove plant</button>";
+  //   plantDiv.append(htmlForPlantCard);
+  // });
 }
-
+ 
+let journal = new Journal();
 
 $(document).ready(function () {
-  attachPlantListeners();
+  //attachPlantListeners();
   $("#addPlant").submit(function () {
     event.preventDefault();
 
@@ -48,9 +34,12 @@ $(document).ready(function () {
     $("input#plantBirthday").val("");
 
     let newPlant = new Plant(inputtedPlantName, inputtedPlantType, inputtedPlantDate, inputtedWaterDay);
-    let journal = new Journal();
+    
     journal.addPlant(newPlant);
-    showPlantCard(journal);
+    console.log(journal.plants);
+    showPlantCard(newPlant);
+    //newPlant.showPlantCard();
+    // showPlantCard(journal);
 
   });
 });
