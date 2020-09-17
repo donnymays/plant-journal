d3f800e117d1e5a104e2470bf9636885d6d8fe1d
donnymays <dtmays2@gmail.com>
2020-09-16 20:08:49 -0700

    plant waterDate method is functional and appending correctly to plant card Co-authored-by: Alma Frankenstein <a.q.frankenstein@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-
-// import  Plant from './plant.js';
 import { Journal, Plant } from './journal.js';
 
 
@@ -11,9 +9,8 @@ function showPlantCard(plant) {
   let plantDiv = $("div#bottomDiv");
   let htmlForPlantCard = "";
 
-  htmlForPlantCard += "<div class='col-md-4'><div class='card plantCard' id='plantCard" + plant.id + "'><div class='card-body'><h5 class='card-title'>" + plant.name + "</h5><h6 id='plantCardType' class='card-subtitle mb-2'>" + plant.type + "</h6><p>" + plant.birthday + "</p><p>" + plant.waterDay + "</p><button class='btn-dark deleteButton' type='button'>Remove plant</button>";
+  htmlForPlantCard += "<div class='col-md-4'><div class='card plantCard' id='plantCard" + plant.id + "'><div class='card-body'><h5 class='card-title'>" + plant.name + "</h5><h6 id='plantCardType' class='card-subtitle mb-2'>" + plant.type + "</h6><p>" + plant.birthday + "</p><p>" + plant.waterDay + "</p><button class='btn-dark deleteButton' type='button'>Remove plant</button><button class='btn-dark waterButton' type='button'>Water plant</button>";
   plantDiv.append(htmlForPlantCard);
-
 }
 
 let journal = new Journal();
@@ -25,18 +22,17 @@ $(document).ready(function () {
     let inputtedPlantName = $("input#plantName").val();
     let inputtedPlantType = $("input:radio[name=plantType]:checked").val();
     let inputtedPlantDate = $("input#plantBirthday").val();
-    let inputtedWaterDay = $("input:radio[name=waterDay]:checked").val();
+    let inputtedWaterDay = parseInt($("input#waterDay").val());
    
     $("input#plantName").val("");
     $("input#plantBirthday").val("");
+    $("input#waterDay").val("");
 
     let newPlant = new Plant(inputtedPlantName, inputtedPlantType, inputtedPlantDate, inputtedWaterDay);
 
-    newPlant.waterDate();
-    
-
+    newPlant.waterDate(inputtedWaterDay);
+   
     journal.addPlant(newPlant);
-    console.log(journal.plants);
     showPlantCard(newPlant);
 
     $(".deleteButton").click(function () {
